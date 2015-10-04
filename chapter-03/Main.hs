2f5e2761d9ff7d4e4f826b132c02f099fcebc82c
Daniel Willenson <dwillens@gmail.com>
2015-10-04 00:20:40 -0400

    Copied chapter-02 to chapter-03

---
+++ chapter-03/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+  import Control.Monad
+  import qualified Data.ByteString.Lazy as LBS
+  import System.Environment
+
+  import Scanner
+
+  scan = do
+    [inputFile] <- getArgs
+    input <- LBS.readFile inputFile
+    let tokens = scanTokens input
+    forM tokens $ putStrLn . show
+
+  main = scan
---
+++ chapter-03/Tokens.hs
@@ -0,0 +1,52 @@
+module Tokens where
+  data Position = Position { offset :: Int, line :: Int, col :: Int }
+    deriving (Eq, Show)
+
+  data Token =
+      While Position
+    | For Position
+    | To Position
+    | Break Position
+    | Let Position
+    | In Position
+    | End Position
+    | Function Position
+    | Var Position
+    | Type Position
+    | Array Position
+    | If Position
+    | Then Position
+    | Else Position
+    | Do Position
+    | Of Position
+    | Nil Position
+
+    | Comma Position
+    | Colon Position
+    | Semicolon Position
+    | LeftParen Position
+    | RightParen Position
+    | BeginSubscript Position
+    | EndSubscript Position
+    | BeginRecord Position
+    | EndRecord Position
+    | Member Position
+    | Minus Position
+    | Plus Position
+    | Times Position
+    | Divide Position
+    | Equals Position
+    | NotEquals Position
+    | Less Position
+    | LessOrEquals Position
+    | Greater Position
+    | GreaterOrEquals Position
+    | And Position
+    | Or Position
+    | Assign Position
+
+    | Int Integer Position
+    | String String Position
+    | Identifier String Position
+
+    deriving (Eq, Show)
