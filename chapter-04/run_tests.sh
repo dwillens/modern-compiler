5c0347bf4f466227a01881e2120acc28c79e1227
Daniel Willenson <dwillens@gmail.com>
2015-10-05 01:19:43 -0400

    Added run_tests script

---
+++ chapter-04/run_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PHASE="${1}"
+shift
+
+for INPUT in $*; do
+  echo -----------------------------
+  echo "${INPUT}"
+  echo -----------------------------
+  if ! grep "${INPUT}" errors.txt; then
+    cat -n "${INPUT}"
+    echo
+    echo -----------------------------
+    dist/build/Tiger/Tiger "${PHASE}" "${INPUT}"
+    if [ "$?" -ne 0 ]; then
+      break
+    fi
+  fi
+done | less -S
