e0abf1def85d6305254301032daeeb187eb72eb8
Daniel Willenson <dwillens@gmail.com>
2015-10-13 01:33:03 -0400

    Refactoring

--- chapter-05/TypeChecker.hs
+++ chapter-05/TypeChecker.hs
@@ -24,6 +24,10 @@ module TypeChecker(typeCheck) where
   type Types = M.Map String Type
   type Environment = (Values, Types)
 
+  typeCheck :: AST.Expression -> Either String ()
+  typeCheck = void . expression (M.empty, standardTypes)
+    where standardTypes = M.fromList [("int", Int), ("string", String)]
+
   reportError :: Show p => p -> String -> Either String a
   reportError p = Left . (++) (show p)
 
@@ -33,9 +37,42 @@ module TypeChecker(typeCheck) where
   insertType :: Environment -> AST.Identifier -> Type -> Environment
   insertType (vs, ts) (name, _) ty = (vs, M.insert name ty ts)
 
-  typeCheck :: AST.Expression -> Either String ()
-  typeCheck = void . expression (M.empty, standardTypes)
-    where standardTypes = M.fromList [("int", Int), ("string", String)]
+
+  typeMatch :: Either String Type -> Type -> Type -> Either String Type
+  typeMatch message a b
+    | a == b = return a
+    | otherwise = message
+
+  lengthsMatch :: Either String () -> [a] -> [b] -> Either String ()
+  lengthsMatch message inits fs
+    | length inits == length fs = return ()
+    | otherwise = message
+
+
+  variable :: Environment -> AST.Variable -> Either String Type
+  variable (vs, _) (AST.SimpleVariable (name, p)) =
+    case M.lookup name vs of
+      Just (Variable t) -> return t
+      Just _ -> reportError p ": expected a variable"
+      Nothing -> reportError p ": could not find variable"
+
+  variable env (AST.SubscriptVariable var index p) =
+    expression env index >>= indexType env
+      >> variable env var >>= arrayType env
+    where indexType _ Int = return Int
+          indexType _ _ = reportError p ": index must be an int"
+          arrayType _ (Array t) = return t
+          arrayType _ _ = reportError p ": subscripted must be an array"
+
+  variable env (AST.FieldVariable record (member, _) p) =
+    variable env record >>= field
+    where field :: Type -> Either String Type
+          field (Record fs) =
+            case lookup member fs of
+              Just ty -> return ty
+              Nothing -> reportError p ": could not find field"
+          field _ = reportError p ": expected a record type"
+
 
   expression :: Environment -> AST.Expression -> Either String Type
   expression env (AST.VariableExpression v) = variable env v
@@ -52,7 +89,7 @@ module TypeChecker(typeCheck) where
   expression env (AST.EqualityExpression _ l r p) =
     do lType <- expression env l
        rType <- expression env r
-       typesMatch matchError lType rType
+       typeMatch matchError lType rType
     where matchError = reportError p ": equality types must match"
 
   expression env (AST.OrderingExpression _ l r p) =
@@ -69,22 +106,21 @@ module TypeChecker(typeCheck) where
     resolveType env ty >>= recordType env fs
     where recordType :: Environment -> [(AST.Identifier, AST.Expression)] -> Type -> Either String Type
           recordType env inits rec@(Record fs) =
-            lengthsMatch inits fs >> zipWithM (fieldType env) fs inits >> return rec
+            do lengthsMatch lengthError inits fs
+               zipWithM (fieldType env) fs inits
+               return rec
           recordType _ _ _ = reportError p ": expected record type"
-          lengthsMatch inits fs
-            | length inits == length fs = return ()
-            | otherwise = reportError p ": wrong number of fields"
-
           fieldType :: Environment -> (String, Type) -> (AST.Identifier, AST.Expression) -> Either String Type
           fieldType env (f, ty) ((name, _), init)
-            | f == name = expression env init >>= typesMatch matchError ty
+            | f == name = expression env init >>= typeMatch matchError ty
             | otherwise = reportError p ": wrong field"
           matchError = reportError p ": field init type must match"
+          lengthError = reportError p ": wrong number of fields"
 
   expression env (AST.AssignExpression to val p) =
     do toType <- variable env to
        valType <- expression env val
-       typesMatch matchError toType valType
+       typeMatch matchError toType valType
        return Unit
     where matchError = reportError p ": assignment types must match"
 
@@ -92,7 +128,7 @@ module TypeChecker(typeCheck) where
     expression env test >>= testType env
       >> do thenType <- expression env thenE
             elseType <- expression env elseE
-            typesMatch matchError thenType elseType
+            typeMatch matchError thenType elseType
     where testType _ Int = return Int
           testType _ _ = reportError p ": if condition must be int"
           matchError = reportError p ": if-then-else types must match"
@@ -143,29 +179,6 @@ module TypeChecker(typeCheck) where
           lastType f p x = p *> f x
 
 
-  variable :: Environment -> AST.Variable -> Either String Type
-  variable (vs, _) (AST.SimpleVariable (name, p)) =
-    case M.lookup name vs of
-      Just (Variable t) -> return t
-      Just _ -> reportError p ": expected a variable"
-      Nothing -> reportError p ": could not find variable"
-
-  variable env (AST.SubscriptVariable var index p) =
-    expression env index >>= indexType env
-      >> variable env var >>= arrayType env
-    where indexType _ Int = return Int
-          indexType _ _ = reportError p ": index must be an int"
-          arrayType _ (Array t) = return t
-          arrayType _ _ = reportError p ": subscripted must be an array"
-
-  variable env (AST.FieldVariable record (member, _) p) =
-    variable env record >>= field
-    where field :: Type -> Either String Type
-          field (Record fs) =
-            case lookup member fs of
-              Just ty -> return ty
-              Nothing -> reportError p ": could not find field"
-          field _ = reportError p ": expected a record type"
 
 
   resolveType :: Environment -> AST.Identifier -> Either String Type
@@ -184,26 +197,23 @@ module TypeChecker(typeCheck) where
   declaration env (AST.VariableDeclaration name (Just ty) init p) =
     do initType <- expression env init
        decType <- resolveType env ty
-       typesMatch matchError initType decType
+       typeMatch matchError initType decType
        return $ insertValue env name $ Variable initType
     where matchError = reportError p ": declaration types must match"
 
-  declaration env (AST.TypeDeclarationGroup ds) = foldM bind env ds
-    where bind :: Environment -> AST.TypeDeclaration -> Either String Environment
-          bind env (AST.TypeDeclaration name ty _) =
-            actual env ty >>= return . insertType env name
-          actual :: Environment -> AST.Type -> Either String Type
+  declaration env (AST.TypeDeclarationGroup ds) = foldM bindType env ds
+
+  declaration _ node = reportError node "\nCan't check declaration"
+
+  bindType :: Environment -> AST.TypeDeclaration -> Either String Environment
+  bindType env (AST.TypeDeclaration name ty _) =
+    actual env ty >>= return . insertType env name
+    where actual :: Environment -> AST.Type -> Either String Type
           actual env (AST.NamedType name) = resolveType env name
           actual env (AST.ArrayType name p) =
             resolveType env name >>= return . Array
           actual env (AST.RecordType fs p) =
-            mapM (field env) fs >>= return . Record
+            forM fs (field env) >>= return . Record
           field env (AST.Field (name, _) ty) =
             resolveType env ty >>= return . (,) name
 
-  declaration _ node = reportError node "\nCan't check declaration"
-
-  typesMatch :: Either String Type -> Type -> Type -> Either String Type
-  typesMatch message a b
-    | a == b = return a
-    | otherwise = message
